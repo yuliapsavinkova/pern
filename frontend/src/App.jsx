a84f70e66e90d30bf2a8cfa873467c2aadd79222
Yulia <yuliasavinkova@gmail.com>
2025-04-10 14:00:38 -0400

    Added canvas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import TradesCanvas from './pages/TradesCanvas';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/canvas" element={<TradesCanvas />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/TradesCanvas.jsx
@@ -0,0 +1,10 @@
+const TradesCanvas = () => {
+  return (
+    <div>
+      <h1>Trades Canvas</h1>
+      <p>PLTR</p>
+    </div>
+  );
+};
+
+export default TradesCanvas;
